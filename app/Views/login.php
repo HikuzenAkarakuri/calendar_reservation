78a21db1fccd1d008716d82528b08dd3ee24e3cc
HikuzenAkarakuri <gustavomazzoncini206@gmail.com>
2023-09-25 19:31:30 -0300

    first

---
+++ app/Views/login.php
@@ -0,0 +1,22 @@
+<?php if(session()->getFlashdata('error')): ?>
+    <div class="alert alert-danger"><?= session()->getFlashdata('error') ?></div>
+<?php endif;?>
+
+<?php if(session()->getFlashdata('success')): ?>
+    <div class="alert alert-success"><?= session()->getFlashdata('success') ?></div>
+<?php endif;?>
+
+
+<form action="/authenticate" method="post">
+    <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="email" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="senha" class="form-control" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
+
+<p>Registre-Se <a href="register"> aqui</a></p>
